bf5e289b5400034cd1d9331f6591ba127452ed77
Lucas <lukalds@gmail.com>
2020-11-30 10:44:28 -0300

    ultimas alterações

--- src/main/java/br/com/docket/restapi/model/Cartorio.java
+++ src/main/java/br/com/docket/restapi/model/Cartorio.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -20,5 +17,7 @@ public class Cartorio {
     private Integer id;
     private String nome;
     private String endereco;
+
+    @Lob
     private String tipoCertidao;
 }
